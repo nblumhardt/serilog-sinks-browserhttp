39bed3dc39ee25487063a8c99f60766db768d5e7
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-11-09 19:38:41 +1000

    Add appveyor.yml, Build.ps1, stub test projects

---
+++ test/Serilog.AspNetCore.Ingestion.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Serilog.AspNetCore.Ingestion.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ test/Serilog.Sinks.BrowserHttp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Serilog.Sinks.BrowserHttp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
